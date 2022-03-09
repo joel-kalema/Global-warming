230e3ec0c5b4a4ccfac3761920be00bf47718e29
joel <joelkalema63@gmail.com>
2022-03-09 18:53:17 +0200

    Addad some linters

--- index.js
+++ index.js
@@ -3,44 +3,44 @@ const speackers = [
     name: 'Joel kama',
     photo: 'image01',
     role: 'Project manager',
-    description: 'Before the 1980s, it was unclear whether warming by increased...'
+    description: 'Before the 1980s, it was unclear whether warming by increased...',
   },
   {
     name: 'Marcus perezz',
     photo: 'image02',
     role: 'Project manager',
-    description: 'Before the 1980s, it was unclear whether warming by increased...'
+    description: 'Before the 1980s, it was unclear whether warming by increased...',
   },
   {
     name: 'Jean de Dieu',
     photo: 'image03',
     role: 'Project manager',
-    description: 'Before the 1980s, it was unclear whether warming by increased...'
+    description: 'Before the 1980s, it was unclear whether warming by increased...',
   },
   {
     name: 'Grisia luiws',
     photo: 'image04',
     role: 'Project manager',
-    description: 'Before the 1980s, it was unclear whether warming by increased...'
+    description: 'Before the 1980s, it was unclear whether warming by increased...',
   },
   {
     name: 'Sham Gomez',
     photo: 'image05',
     role: 'Project manager',
-    description: 'Before the 1980s, it was unclear whether warming by increased...'
+    description: 'Before the 1980s, it was unclear whether warming by increased...',
   },
   {
     name: 'Michal Rio',
     photo: 'image06',
     role: 'Project manager',
-    description: 'Before the 1980s, it was unclear whether warming by increased...'
-  }
+    description: 'Before the 1980s, it was unclear whether warming by increased...',
+  },
 ];
 
 window.addEventListener('scroll', () => {
   const nav = document.querySelector('.hambager');
   const navBar = document.querySelector('nav');
-  const icon = document.querySelector('.icon')
+  const icon = document.querySelector('.icon');
   nav.classList.toggle('scroll', window.scrollY > 0);
   navBar.classList.toggle('desc-scrool', window.scrollY > 0);
   icon.classList.toggle('active', window.scrollY > 0);
@@ -48,27 +48,20 @@ window.addEventListener('scroll', () => {
 
 const navOpen = document.querySelectorAll('.toggle');
 const nav = document.querySelector('.nav-links');
-const closeNav  = document.querySelectorAll('.toggle-close')
+const closeNav = document.querySelectorAll('.toggle-close');
 
 navOpen.forEach((btn) => {
-  btn.addEventListener('click',() => {
+  btn.addEventListener('click', () => {
     nav.classList.toggle('nav-active');
   });
 });
 
 closeNav.forEach((close) => {
-  close.addEventListener('click',() => {
+  close.addEventListener('click', () => {
     nav.classList.remove('nav-active');
   });
 });
 
-const moreSee = () =>{
-  const chowSpeacer = document.querySelector('.speaker-data');
-const more = document.querySelector('.more');
-more.addEventListener('click', () => {
-  chowSpeacer.classList.toggle('speaker-dataAll');
-});
-}
 const mainProjectsContainer = document.querySelector('.speaker');
 mainProjectsContainer.innerHTML += speackers.map((project) => (`<div class="speaker-data">
                                                                 <div class="${project.photo}"></div>
